885dfeaa88c165ec603cd4a1c27ae88b49026f1a
HungyeeLinS3790956 <S3790956@student.rmit.edu.au>
2023-01-05 20:02:44 +0800

    test send email method

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const cors = require('cors')
 const fs = require('fs')
 const bodyParser = require('body-parser')
 const reader = require('xlsx')
+const nodemailer = require('nodemailer');
 
 
 const app = express()
-const port = process.env.PORT|| 3000
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 app.use(cors())
@@ -21,11 +22,40 @@ app.get('/', (req, res) => {
 
 app.get('/info/:type', (req, res) => {
     const file = reader.readFile('./DB.xlsx')
-    let type = req.params.type==='items'?0:1
+    let type = req.params.type === 'items' ? 0 : 1
     let data = []
 
-    reader.utils.sheet_to_json(file.Sheets[file.SheetNames[type]]).map(item=>data.push(item))
+    reader.utils.sheet_to_json(file.Sheets[file.SheetNames[type]]).map(item => data.push(item))
     res.json(data)
+})
+
+app.get('/notify', (req, res) => {
+    const myEmail = 'hungyeelin@gmail.com';
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: myEmail,
+            pass: 'rmhqwkdkgnrfhdug'
+        }
+    });
+
+    const mailOptions = {
+        from: myEmail,
+        to: 'hungyeelin@gmail.com',
+        subject: 'TestOrderProject',
+        text: 'hahahahahaha'
+    };
+
+    transporter.sendMail(mailOptions, (error, info)=> {
+        if (error) {
+            console.log(error);
+        }else{
+            console.log(info)
+        }
+    });
+
+    res.json('finish it!')
 })
 
 app.listen(port, () => {
