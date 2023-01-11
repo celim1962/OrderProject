65f269aa07115c4a02bc44993aa4a0efd9e92e7f
HungyeeLinS3790956 <S3790956@student.rmit.edu.au>
2023-01-11 13:27:03 +0800

    fix email content

--- app.js
+++ app.js
@@ -32,23 +32,30 @@ app.get('/info/:type', (req, res) => {
 
 app.post('/notify', (req, res) => {
     let result = true;
-
-    let body = req.body;
-    let content = `親愛的貴賓 ${body.keyinfo.name} 先生/小姐 您好\n您的訂購資訊如下:
-    \n------------------\n`;
-
     let total = 0;
+    let body = req.body;
+
+    let content = `親愛的貴賓 ${body.keyinfo.name} 先生/小姐 您好\n`;
+    content += `您的訂購資訊如下:\n`;
+    content += `------------------\n`;
 
     body.data.map(item => {
         content += `${item.name} x${item.count} =  $${item.price * item.count}\n`;
         total += item.price * item.count
     })
 
-    content += `------------------\n\n連絡電話: ${body.keyinfo.phone}\n`;
-    content += `地址: ${body.keyinfo.address}\n`;
+    content += `\n總計金額為 $${total}\n`;
+    content += `------------------\n`;
+    content += `匯款資訊:\n`;
+    content += ` 代號:700\n`;
+    content += ` 存簿帳號:0111000 1048835\n\n`;
+    content += `連絡電話: ${body.keyinfo.phone}\n`;
+    content += `寄送地址: ${body.keyinfo.address}\n`;
     content += `備註: ${body.keyinfo.notes}\n`;
-    content += `------------------\n總計金額為 $${total}\n\n感謝您的訂購，稍後門市人員會致電聯絡付款事項\n多謝!\n\nOrderProject 團隊`;
-
+    content += `------------------\n`;
+    content += `感謝您的訂購，請參照上述匯款資訊，門市會在確認匯款成功後透過宅配發貨\n`;
+    content += `多謝!\n\n`;
+    content += `喜福宅鮮 團隊`;
 
     let sender = 'amazingfood7777@gmail.com';//'hungyeelin@gmail.com';
 
@@ -63,7 +70,7 @@ app.post('/notify', (req, res) => {
     const mailOptions = {
         from: process.env.sender || sender,
         to: body.keyinfo.email,
-        subject: 'OrderProject訂購測試信件(此為系統信件請勿回復)',
+        subject: '喜福宅鮮 訂購信件(測試用)',
         text: content
     };
 
@@ -71,11 +78,24 @@ app.post('/notify', (req, res) => {
 
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
-            return res.send(false)
-        } else {
-            return res.json(info)
-        }
+            console.log(error)
+        } 
     });
+
+    // const mailOptions2 = {
+    //     from: process.env.sender || sender,
+    //     to: process.env.sender || sender,
+    //     subject: '喜福宅鮮 訂購信件(測試用)',
+    //     text: content
+    // };
+
+    // transporter.sendMail(mailOptions2, (error, info) => {
+    //     if (error) {
+    //         console.log(error)
+    //     } 
+    // });
+
+    return res.json('finish')
 
 
 
